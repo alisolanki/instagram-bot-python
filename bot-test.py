e77f31843943cc2e6aaa88c2bcb6392c54b297aa
Rishabh Pahwa <74728374+Rishabh-Pahwa@users.noreply.github.com>
2023-03-24 03:23:36 +0530

    Add files via upload

--- bot-test.py
+++ bot-test.py
@@ -4,7 +4,7 @@ import customtkinter
 #root_tk =customtkinter.CTk()
 
 window = customtkinter.CTk()  # create CTk window like you do with the Tk window
-window.geometry("900x500")
+window.geometry("1000x600")
 #window = tk.Tk()
 window.title("Instagram Messenger Bot")
 #window.configure(bg="#494949")
@@ -32,7 +32,9 @@ label1 = customtkinter.CTkLabel(master=window,
 label1.place(relx=0.2, rely=0.2, anchor=tk.CENTER)
 
 #---Your username entry box
-entry1 = customtkinter.CTkEntry(master=window, placeholder_text="Enter your username")
+entry1 = customtkinter.CTkEntry(master=window, placeholder_text="Enter your username",
+                                height=35,
+                                width=200)
 #entry1.pack(padx=20, pady=10)
 entry1.place(relx=0.12, rely=0.25)
 
@@ -53,7 +55,9 @@ label2 = customtkinter.CTkLabel(master=window,
 label2.place(relx=0.6, rely=0.2, anchor=tk.CENTER)
 
 #---Your password entry box
-entry2 = customtkinter.CTkEntry(master=window, placeholder_text="Enter your password")
+entry2 = customtkinter.CTkEntry(master=window, placeholder_text="Enter your password",
+                                height=35,
+                                width=200)
 #entry2.pack(padx=20, pady=10)
 entry2.place(relx=0.52, rely=0.25)
 
@@ -72,12 +76,15 @@ label3 = customtkinter.CTkLabel(master=window,
                                fg_color=("transparent"),
                                corner_radius=8)
 #label3.pack(padx=400, pady=200)
-label3.place(relx=0.1, rely=0.35, anchor=tk.NW)
+label3.place(relx=0.1, rely=0.325, anchor=tk.NW)
 
 #---Message entry box
-entry3 = customtkinter.CTkEntry(master=window, placeholder_text="Enter message to be sent")
+entry3 = customtkinter.CTkEntry(master=window, 
+                                placeholder_text="Enter message to be sent",
+                                width=750,
+                                height=100)
 #entry3.pack(padx=200, pady=100)
-entry3.place(relx=0.12, rely=0.425)
+entry3.place(relx=0.12, rely=0.4, anchor=tk.NW)
 
 
 #--4.0-------ENTER  RECIEPENT'S  USERNAME-------------
@@ -93,12 +100,14 @@ label4 = customtkinter.CTkLabel(master=window,
                                fg_color=("transparent"),
                                corner_radius=8)
 #label4.pack(padx=40, pady=20)
-label4.place(relx=0.24, rely=0.525, anchor=tk.CENTER)
+label4.place(relx=0.24, rely=0.62, anchor=tk.CENTER)
 
 #---Reciever username entry box
-entry4 = customtkinter.CTkEntry(master=window, placeholder_text="Enter reciepent's username")
+entry4 = customtkinter.CTkEntry(master=window, placeholder_text="Enter reciepent's username(s)",
+                                height=60,
+                                width=400)
 #entry4.pack(padx=40, pady=20)
-entry4.place(relx=0.12, rely=0.575)
+entry4.place(relx=0.12, rely=0.675)
 
 
 #--5.0-------SUBMIT BUTTON----------
@@ -113,7 +122,7 @@ button1 = customtkinter.CTkButton(master=window,
                                  text=" GO ",
                                 #command=button_event
                                 )
-button1.place(relx=0.5, rely=0.8, anchor=tk.CENTER)
+button1.place(relx=0.5, rely=0.85, anchor=tk.CENTER)
 
 
 window.mainloop()
